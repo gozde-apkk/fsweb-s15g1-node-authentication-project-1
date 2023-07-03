784ef1389c9f605ee420618623fa79228b3185a6
gozde-apkk <gozdeapak.ga.1@gmail.com>
2023-07-03 16:18:34 +0300

    Users-model

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -1,29 +1,46 @@
 /**
   tüm kullanıcıları içeren bir DİZİ ye çözümlenir, tüm kullanıcılar { user_id, username } içerir
  */
-function bul() {
 
+const db = require("../../data/db-config");
+
+
+function bul() {
+    return db("users");
 }
 
 /**
   verilen filtreye sahip tüm kullanıcıları içeren bir DİZİ ye çözümlenir
  */
 function goreBul(filtre) {
-
+    return db("users").where(filtre);
 }
 
 /**
   verilen user_id li kullanıcıya çözümlenir, kullanıcı { user_id, username } içerir
  */
-function idyeGoreBul(user_id) {
-
+async function idyeGoreBul(user_id) {
+      let user = await db("users").where({user_id}).first();
+      return {user_id: user.user_id , username : user.username};
 }
 
 /**
   yeni eklenen kullanıcıya çözümlenir { user_id, username }
  */
-function ekle(user) {
+async  function ekle(user) {
+  const newUserId = await db("users").insert(user);
+  const newUser =await idyeGoreBul(newUserId);
+  return {
+    user_id : newUser.user_id,
+    username:newUser.username
+  }
 
 }
 
 // Diğer modüllerde kullanılabilmesi için fonksiyonları "exports" nesnesine eklemeyi unutmayın.
+
+
+module.exports={
+  bul,
+  ekle,idyeGoreBul,goreBul
+}
