857dad9027177b429e1a58d24daca4a8a0be9861
gozde-apkk <gozdeapak.ga.1@gmail.com>
2023-07-03 20:04:03 +0300

    changes

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,3 +1,8 @@
+
+
+
+const UserModel = require("../users/users-model");
+
 /*
   Kullanıcının sunucuda kayıtlı bir oturumu yoksa
 
@@ -6,7 +11,20 @@
     "message": "Geçemezsiniz!"
   }
 */
-function sinirli() {
+async  function sinirli(req,res,next) {
+  try{
+    if(req.session.user){
+      next();
+    }else{
+      next({
+        status : 401,
+        message:"Geçemezsiniz!"
+      })
+    }
+
+  }catch(error){
+    next(error);
+  }
 
 }
 
@@ -18,7 +36,20 @@ function sinirli() {
     "message": "Username kullaniliyor"
   }
 */
-function usernameBostami() {
+async function usernameBostami(req,res,next) {
+     try{
+      const {username} = req.body;
+      const existUser = await UserModel.goreBul({username: req.body.username})
+      if(existUser.length > 0){
+        res.status(422).json({message:"Username kullaniliyor"})
+      }else{
+        next()
+      }
+    }catch(error){
+      next(error);  
+    }
+
+  
 
 }
 
@@ -43,7 +74,12 @@ function usernameVarmi() {
   }
 */
 function sifreGecerlimi() {
-
+    const {password} = req.body;
+    if(!password || password.length <3){
+      res.status(422).json({message:"Şifre 3 karakterden fazla olmamlı"})
+    }else{
+      const hashedPassword = bcrypt.hashedPassword
+    }
 }
 
 // Diğer modüllerde kullanılabilmesi için fonksiyonları "exports" nesnesine eklemeyi unutmayın.
--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,7 +1,9 @@
 // `checkUsernameFree`, `checkUsernameExists` ve `checkPasswordLength` gereklidir (require)
 // `auth-middleware.js` deki middleware fonksiyonları. Bunlara burda ihtiyacınız var!
 
-
+const router = require("express").Router();
+const UserModel = require()
+const bcrypt = 
 /**
   1 [POST] /api/auth/register { "username": "sue", "password": "1234" }
 
@@ -26,6 +28,16 @@
  */
 
 
+  router.post("/register", usernameBostami, sifreGercerlimi, (req,res,next) =>{
+        const {username} = req.body;
+        try{
+            await UserModel.ekle({username: username , password : req.hash});
+            res.status(201).json(user)
+        }catch(error){
+          next(error);
+        }
+  })
+
 /**
   2 [POST] /api/auth/login { "username": "sue", "password": "1234" }
 
@@ -41,7 +53,19 @@
     "message": "Geçersiz kriter!"
   }
  */
-
+router.post("/login" ,usernameVarmi, (req,res,next)) => {
+  const {username , password} = req.body;
+ try{
+  if(req.user && bcrypt.compareSync(password, req.user.password) ){
+    req.session.user = req.user;
+    res.status(200).json({message: "Hoşgeldin "})
+  }else{
+    res.status(401).json({message:"Geçersiz kriter!"})
+  }
+ }catch(error){
+  next(error);
+ }
+}
 
 /**
   3 [GET] /api/auth/logout
@@ -58,6 +82,15 @@
     "message": "Oturum bulunamadı!"
   }
  */
+router.get("/logout" , (res) =>{
+  if(req.session && req.session.user){
+    req.session.destroy (error =>{
+      if(error){
 
+      }else{
+        res.set("Set-Cookie" , "fadfadfa")
+      }    })
+  }
+})
  
 // Diğer modüllerde kullanılabilmesi için routerı "exports" nesnesine eklemeyi unutmayın.
